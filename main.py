d4eef76fd8c9043d07668a194418aa6fc3cb62fe
Kristiyan Georgiev <2003kristiyan@gmail.com>
2025-10-07 01:11:31 +0200

    Update installer

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from gooey import Gooey, GooeyParser
 from pathlib import Path
+from zipfile import ZipFile
 import os
-import pyxdelta
 import requests
 import shutil
-from zipfile import ZipFile
+import xdelta3
+
+VERSION = "1.1.0"
 
 # Needed for Gnome to work properly
 os.environ['GTK_THEME'] = 'Adwaita:light'
@@ -12,10 +14,13 @@ os.environ['GTK_THEME'] = 'Adwaita:light'
 # Handle arguments in GUI
 @Gooey(
         default_size=(650, 500),
-        program_name='LBEE Restoration Patch',
+        program_name=f'LBEE Restoration Patch v{VERSION}',
         program_description="Restore the original look and feel of 'Little Busters English Edition'",
         show_restart_button=False,
-        image_dir='assets')
+        image_dir='assets/gooey',
+        progress_regex=r"\((?P<progress>\d+)/(?P<total>\d+)\)$",
+        progress_expr="progress / total * 100"
+        )
 def main():
     # Define variables (OS specific)
     if os.name == 'nt':
@@ -25,11 +30,14 @@ def main():
         lucksystem = Path("./LuckSystem_linux")
         lucksystem_url = 'http://github.com/wetor/LuckSystem/releases/latest/download/LuckSystem_linux_x86_64.zip'
 
-    source = Path("./source")
-    source_url = 'https://github.com/Danar435/lbee-restoration/archive/refs/tags/v1.0.2.zip'
+    source = Path(f"./lbee-restoration-{VERSION}/source")
+    source_url = f'https://github.com/Danar435/lbee-restoration/archive/refs/tags/v{VERSION}.zip'
 
     # List of paks to repack
-    pak_list = [ "battle", "bgcg", "charcg", "eventcg", "gencg", "gm", "othcg", "parts", "pt", "syscg", "script" ]
+    pak_list = [ "battle", "bgcg", "charcg", "eventcg", "gencg", "gm", 
+                "othcg", "parts", "pt", "syscg", "script" ]
+    uncensored_list = ["othcg", "eventcg", "script" ]
+    suginami_list =  ["charcg", "script" ]
 
     # Set up the parser
     parser = GooeyParser()
@@ -43,7 +51,7 @@ def main():
 
     # Optional arguments
     options = parser.add_argument_group("Optional Settings", gooey_options={'show_border': True})
-    options.add_argument('-c', '--censored', 
+    options.add_argument('-u', '--uncensored', 
                          metavar='Uncensored Assets', 
                          help="Use the original uncensored assets", 
                          action="store_false",
@@ -61,94 +69,133 @@ def main():
     args = parser.parse_args()
     input = Path(args.path)
     exe = Path(f"{input}/LITBUS_WIN32.exe")
-    exe_backup = Path(f"{exe}-backup")
-
-    # Error catching
-    try:
-        exe.resolve(strict=True)
-
-    except:
+    exe_backup = Path(f"{input}/LITBUS_WIN32-backup.exe")
+    
+    # Check if the path is right
+    if not exe.exists():
         print("🟥 LITBUS_WIN32.exe not found!")
-        print("Make sure that the game path is correct.")
-        print("It should point to 'Little Busters! English Edition'.")
+        print("ℹ️ Make sure that the game path is correct. " \
+        "It should point to the folder 'Little Busters! English Edition'.")
         exit(1)
 
-    # Download any missing assets
-    download(lucksystem, lucksystem_url)
-    download(source, source_url)
-
-    # Run the main repack script
-    print("➡️ Patching main assets...")
-    for i in pak_list:
-        repack(lucksystem, source, input, i)
-
-    # Handle uncensoring assets
-    if not args.censored:
-        print("➡️ Patching uncensored assets...")
-        for i in ["othcg", "eventcg", "script" ]:
-            repack(lucksystem, source / "auxiliary-files" / "uncensored", input, i)
-
-    # Handle the Suginami mod
+    # Set up progress variables
+    progress = 0
+    total = len(pak_list)
+    if not args.uncensored:
+        total += len(uncensored_list)
     if not args.suginami:
-        print("➡️ Patching Suginami assets...")
-        for i in ["charcg", "script"]:
-            repack(lucksystem, source / "auxiliary-files" / "suginami", input, i)
+        total += len(suginami_list)
+
+    # Download lucksystem
+    if not lucksystem.exists():
+        # Download via requests
+        print(f"⬇️ Downloading LuckSystem...")
+        check_internet()
+        response = requests.get(lucksystem_url, allow_redirects=True)
+        open("lucksystem.zip", 'wb').write(response.content)
+
+        # Extract via zipfile
+        with ZipFile("lucksystem.zip", 'r') as zObject:
+            zObject.extract(lucksystem.name)
+        os.remove("lucksystem.zip")
+        if os.name != 'nt':
+            os.chmod(lucksystem, 0o755)
+    else:
+        print("☑️ LuckSystem already downloaded!")
+
+    # Download the source
+    if not source.exists():
+        # Download via requests
+        print(f"⬇️ Downloading the assets...")
+        check_internet()
+        with requests.get(source_url, stream=True, allow_redirects=True) as r:
+            r.raise_for_status()
+            with open("source.zip", 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+
+        # Extract via zipfile
+        with ZipFile("source.zip", 'r') as zObject:
+            for member in zObject.namelist():
+                if member.startswith(str(source)):
+                    zObject.extract(member)
+        os.remove("source.zip")
+    else:
+        print(f"☑️ The assets are already downloaded!")
 
     # Patch the exe
     print("ℹ️ Patching the executable...")
-    try:
-        exe_backup.resolve(strict=True)
-    except:
+    
+    if not exe_backup.exists():
         shutil.copy(exe, exe_backup)
-        
-    pyxdelta.decode(str(exe_backup), str(source / "auxiliary-files" / "LITBUS_WIN32.xdelta"), str(exe))
+    try:
+        with open(exe_backup, "rb") as f:
+            original_data = f.read()
+        with open(source / "auxiliary-files" / "LITBUS_WIN32.xdelta", "rb") as f:
+            patch_data = f.read()
 
-    # Patch the movies
-    print("ℹ️ Patching the movies...")
-    shutil.copytree(source / "auxiliary-files" / "movie", input / "files", dirs_exist_ok=True)
+        decoded_data = xdelta3.decode(original_data, patch_data)
+
+        with open(exe, "wb") as f:
+            f.write(decoded_data)
+
+    except:
+        print("🟥 Failed to patch the executable!")
+        print("ℹ️ Make sure that you are using a legitimate copy of LBEE. Any recent updates may break " \
+        "the patch. If you have used this patch before and have deleted 'LITBUS_WIN32-backup.exe', " \
+        "then please verify game files within Steam and run the patch again.")
+        exit(1)
 
     # Patch the config
     print("ℹ️ Patching the config...")
     shutil.copy(source / "auxiliary-files" / "system.cnf", input)
 
-    # Fix CHARCG.PAK
+    # Patch the movies
+    print("ℹ️ Patching the movies...")
+    shutil.copytree(source / "auxiliary-files" / "movie", input / "files" / "movie", dirs_exist_ok=True)
+
+    # Run the main repack script
+    print("➡️ Patching main assets...")
+    for i in pak_list:
+        progress += 1
+        repack(lucksystem, source, input, i, progress, total)
+
+    # Handle uncensoring assets
+    if not args.uncensored:
+        print("➡️ Patching uncensored assets...")
+        for i in uncensored_list:
+            progress += 1
+            repack(lucksystem, source / "auxiliary-files" / "uncensored", input, i, progress, total)
+
+    # Handle the Suginami mod
+    if not args.suginami:
+        print("➡️ Patching Suginami assets...")
+        for i in suginami_list:
+            progress += 1
+            repack(lucksystem, source / "auxiliary-files" / "suginami", input, i, progress, total)
+
+    # Remove overlays in characters pak
     print("ℹ️ Fixing CHARCG.PAK...")
     with open(input / "files" / "CHARCG.PAK", "r+b") as file:
         file.seek(0x9568)
         file.write(b"\x00" * (0xA42C - 0x9568))
 
+    # Finish
     print("✅ Patching completed!")
 
-def download(input, url):
+def check_internet():
     try:
-        # Check if file exists
-        input.resolve(strict=True)
+        response = requests.get('https://www.google.com/', timeout=5)
+        return
+    except (requests.ConnectionError, requests.Timeout):
+        print("🟥 Failed to connect to internet!")
+        print("ℹ️ If you want to use the installer offline, then download the source code and " \
+            "lucksystem beforhand. Afterwards place the 'source' folder and 'LuckSystem_windows.exe' " \
+            "or 'LuckSystem_linux' binary in the same folder as this patch.")
+        exit(1)
 
-    except:
-        # Download via requests
-        print(f"⬇️ Downloading {str(input).upper()}...")
-        try:
-            response = requests.get(url, allow_redirects=True)
-            response.raise_for_status()
-        except:
-            print(f"🟥 Failed to download {url}.")
-            print("If you want to use the installer offline, then download the source code and " \
-                "lucksystem beforhand. Afterwards place the 'source' folder and 'LuckSystem_windows.exe' " \
-                "or 'LuckSystem_linux' in the same folder as this patch")
-            exit(1)
-        open(input.with_suffix('.zip'), 'wb').write(response.content)
-
-        # Extract via zipfile
-        with ZipFile(input.with_suffix('.zip'), 'r') as zObject:
-            zObject.extract(input.name)
-        os.remove(input.with_suffix('.zip'))
-        if os.name != 'nt':
-            os.chmod(input, 0o755)
-
-    else:
-        print(f"☑️ {str(input).upper()} already downloaded")
-
-def repack(lucksystem, source, input, file):
+def repack(lucksystem, source, input, file, progress, total):
     # Define paths
     pak = f"{file.upper()}.PAK"
     pak_input = Path(f"{source}/{file}-done/")
@@ -156,16 +203,14 @@ def repack(lucksystem, source, input, file):
     pak_source = Path(f"{input}/files/{pak}")
 
     # Error catching
-    try:
-        pak_source.resolve(strict=True)
-
-    except:
+    if not pak_source.exists():
         print(f"🟥 {pak} not found!")
+        print("ℹ️ Please verify game files within Steam and run the patch again.")
         exit(1)
 
     # Run lucksystem and replace original file
-    print(f"ℹ️ Repacking {pak}...")
-    os.system(f'"{lucksystem}" pak replace \
+    print(f"ℹ️ Patching {pak}... ({progress}/{total})")
+    os.system(f'"./{lucksystem}" pak replace \
               -s "{pak_source}" \
               -i "{pak_input}" \
               -o "{pak_output}"')
